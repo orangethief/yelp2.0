0fa179377e1743314f826bca35b977ac66916e27
Ruth Meissner <ruthmeissner96@gmail.com>
2024-08-10 13:14:11 +0200

    added fetch request to restaurantList component

--- src/components/Restaurantlist.jsx
+++ src/components/Restaurantlist.jsx
@@ -1,11 +1,33 @@
 import { useState, useEffect } from 'react';
 import RestaurantItem from './RestaurantItem.jsx';
 
-const Restaurantlist = ({restaurants}) => {
+const Restaurantlist = () => {
+    const [restaurantList, setRestaurantList] = useState([]);
+
+    useEffect(() => {
+        let ignore = false;
+        const getRestaurants = async () => {
+            try {
+                const res = await fetch("/src/restaurants.json");
+                const data = await res.json();
+                if (!ignore) {
+                    setRestaurantList(data.restaurants);
+                }
+            } catch (error) {
+                console.log("Error fetching data", error)
+            }
+        }
+        getRestaurants();
+
+        return () => {
+            ignore = true;
+        }
+    }, [])
+
     return (
         <>
         <section id='restaurants-container' className='grid grid-cols-3 gap-4'>
-            {restaurants.map(restaurant => {
+            {restaurantList.map(restaurant => {
                 return <RestaurantItem key={restaurant.id} restaurant={restaurant}/>
             })}
         </section>
