fbebec74e6c92bfc3a5465c9872226076beaa9da
AryKR <dev.krykunenko@gmail.com>
2024-08-10 11:42:03 +0200

    Footer and Header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={footerStyle}>
+      <p>&copy; 2024 Mini-Yelp. All rights reserved.</p>
+    </footer>
+  );
+}
+
+const footerStyle = {
+  backgroundColor: '#333',
+  color: 'white',
+  padding: '10px 0',
+  textAlign: 'center',
+  position: 'fixed',
+  width: '100%',
+  bottom: 0
+};
+
+export default Footer;
