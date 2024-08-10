3bccff6615307b04cdcaf66f3556ad9b798413c5
Lisa <helbig-lisa@web.de>
2024-08-10 14:04:52 +0200

    homepage design ready

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,14 @@
 /* import { Link } from 'react-router-dom' */
 
 import Hero from "../components/Hero"
+import Searchbar from "../components/Searchbar" 
 
 const HomePage = () => {
   return (
     <>
       <Hero/>
+      <Searchbar/>
+
     </>
     //here we will add the components for the home page
   )
