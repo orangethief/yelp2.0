c79abe0df886d65b0c8d24740114b7cdd5ebc8e1
Lisa <helbig-lisa@web.de>
2024-08-10 15:03:38 +0200

    design of Restaurantlist

---
+++ src/components/Filter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Filter = () => {
+  return (
+    <>
+    <section className="flex justify-left m-5">
+    <h3 className="font-bold text-xl mr-2">Filter</h3>
+    <button className="border-2 border-black rounded-xl px-1 bg-white m-1">by Name</button>
+    <button className="border-2 border-black rounded-xl px-1 bg-white m-1">by City</button>
+    <button className="border-2 border-black rounded-xl px-1 bg-white m-1">by Tag</button>
+    </section>
+    </>
+  )
+}
+
+export default Filter
